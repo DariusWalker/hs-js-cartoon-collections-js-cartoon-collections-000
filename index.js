fe8a4c62ec4faf37efe3955cf2c9455338d8d556
Darius Walker <dw862716@ohio.edu>
2020-06-22 14:34:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+
+
+
+
+function dwarfRollCall(dwarves) {
+  let orderedDwarves= []
+  for (let i = 0; i < dwarves.length; i++){
+    orderedDwarves.push(`${i+1}. ${dwarves[i]} `)  
+}
+return orderedDwarves.join("")
+
+}
+
+
+
+function summonCaptainPlanet(planeteerCalls){
+return planeteerCalls.map(a=>a.toUpperCase()+"!")
+ 
+}
+
+function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    if(words[i].length>4)
+    {return true}
+else{return false}
+}
+}
+
+
+function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++){
+    if (foods[i] === 'gouda' || foods[i]=== 'cheddar' || foods[i] ==='camembert') {
+      return foods[i];
+    }
+  }
+  {return 'no cheese!'}
+}
 
